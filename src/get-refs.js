2c4b81a9ca8fb248f5893edec15ffaf5057b8d24
leonid12z <lelik_ceh@ukr.net>
2022-03-20 17:15:30 +0200

    add-files-10

--- src/get-refs.js
+++
@@ -1,7 +0,0 @@
-export default function getRefs() {
-  return {
-    input: document.querySelector('#search-box'),
-    countryInfo: document.querySelector('.country-info'),
-    countryList: document.querySelector('.country-list'),
-  };
-}
